4914a0daf2aafa6f3a92d6bdaaa353e83e9f3809
stautisabela <stautisabela@gmail.com>
2025-07-29 16:06:06 +0200

    Include navbar

---
+++ client/src/Components/Assets/all_products.js
@@ -0,0 +1,39 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+
+let all_products = [
+    {
+        id: 1,
+        name: "aaaaa",
+        category: "women",
+        image: p1_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 2,
+        name: "bbbbb",
+        category: "men",
+        image: p2_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 3,
+        name: "ccccc",
+        category: "kid",
+        image: p3_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 4,
+        name: "ddddd",
+        category: "women",
+        image: p4_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+];
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} width="50px" alt="" />
+            <p>SHOP NAME</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>SHOP</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("cat1")}}><Link style={{ textDecoration: 'none' }} to='/cat1'>CATEGORY 1</Link>{menu==="cat1"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("cat2")}}><Link style={{ textDecoration: 'none' }} to='/cat2'>CATEGORY 2</Link>{menu==="cat2"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("cat3")}}><Link style={{ textDecoration: 'none' }} to='/cat3'>CATEGORY 3</Link>{menu==="cat3"?<hr/>:<></>}</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} width="50px" alt=''/></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
+  )
+}
