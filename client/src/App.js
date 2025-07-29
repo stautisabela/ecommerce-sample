af657cad8e899f4ed72e0082a031c62e1938af36
stautisabela <stautisabela@gmail.com>
2025-07-29 16:06:44 +0200

    Create pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/cat1' element={<ShopCategory category="cat1"/>}/>
+        <Route path='/cat2' element={<ShopCategory category="cat2"/>}/>
+        <Route path='/cat3' element={<ShopCategory category="cat3"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+      
 
     </div>
   );
