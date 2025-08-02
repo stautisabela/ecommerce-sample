af6c7db724b2ce2692ae74597b5c148bf8f6e983
stautisabela <stautisabela@gmail.com>
2025-08-02 13:20:16 +0200

    Add login form

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/cat1' element={<ShopCategory category="cat1"/>}/>
-        <Route path='/cat2' element={<ShopCategory category="cat2"/>}/>
-        <Route path='/cat3' element={<ShopCategory category="cat3"/>}/>
+        <Route path='/cat1' element={<ShopCategory banner={men_banner} category="cat1"/>}/>
+        <Route path='/cat2' element={<ShopCategory banner={women_banner} category="cat2"/>}/>
+        <Route path='/cat3' element={<ShopCategory banner={kids_banner} category="cat3"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
