c4bfce68f4aa324bb27004353faeed47bb13e225
stautisabela <stautisabela@gmail.com>
2025-08-03 07:03:38 +0200

    Add breadcrumbs and product display

--- client/src/Pages/Product.jsx
+++ client/src/Pages/Product.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 export const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e)=> e.id === Number(productId))
+
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrumb product={product}/>
+      <ProductDisplay product={product}/>
+    </div>
   )
 }
