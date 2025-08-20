78765096957b2c373a5558d0622c175aff248b18
stautisabela <stautisabela@gmail.com>
2025-08-20 14:38:50 +0200

    Enable image uploading

--- server/index.js
+++ server/index.js
@@ -19,6 +19,24 @@ app.get("/", (req,res)=>{
     res.send("Express App is running.")
 })
 
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: './uploads/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage})
+
+// Image upload endpoint
+app.use('/images',express.static('uploads/images'))
+app.post("/uploads",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${PORT}/images/${req.file.filename}`
+    })
+})
+
 app.listen(PORT,(error)=> {
     if (!error) {
         console.log("Server Running on Port "+PORT)
