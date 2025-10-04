df767be3a6236d41c1d8d20c00fabab14b791b4e
stautisabela <stautisabela@gmail.com>
2025-10-04 16:20:37 +0200

    implement logout

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -31,7 +31,9 @@ export const Navbar = () => {
             <li onClick={()=>{setMenu("cat3")}}><Link style={{ textDecoration: 'none' }} to='/cat3'>CATEGORY 3</Link>{menu==="cat3"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <Link to='/login'><button>Login</button></Link>
+            {localStorage.getItem('auth-token')
+            ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+            :<Link to='/login'><button>Login</button></Link>}
             <Link to='/cart'><img src={cart_icon} width="50px" alt=''/></Link>
             <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
