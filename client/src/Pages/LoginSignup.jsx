353097b01511cca69946a03142a9f4b641320209
stautisabela <stautisabela@gmail.com>
2025-10-04 16:13:18 +0200

    implement login function

--- client/src/Pages/LoginSignup.jsx
+++ client/src/Pages/LoginSignup.jsx
@@ -17,6 +17,23 @@ export const LoginSignup = () => {
 
   const login = async ()=>{
     console.log("Login function executed", formData)
+    let responseData;
+    
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((res)=>res.json()).then((data)=>{responseData=data});
+
+    if (!responseData.success) {
+      alert(responseData.error);
+      return;
+    }
+  localStorage.setItem('auth-token',responseData.token);
+  window.location.replace('/');
   }
 
   const signup = async ()=>{
